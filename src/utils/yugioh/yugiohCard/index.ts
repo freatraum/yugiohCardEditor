9b1cd94bea639d6e8edbbf46db018aa8c8c430fe
JoneGu <2899601437@qq.com>
2024-08-02 16:34:05 +0800

    card

--- src/utils/yugioh/yugiohCard/index.ts
+++ src/utils/yugioh/yugiohCard/index.ts
@@ -12,15 +12,14 @@ import custom2Style from "./style/custom2-style";
 import astralStyle from "./style/astral-style";
 import { IYGOCard } from "../card/interface";
 import { IUI } from "@leafer-ui/interface";
-import { CSSProperties } from "react";
 
 export class YugiohCard extends Card {
   cardLeaf: Image | null;
   nameLeaf: IUI |null;
   attributeLeaf: null | Image;
-  levelLeaf: null;
-  rankLeaf: null;
-  spellTrapLeaf: null;
+  levelLeaf: null | Group;
+  rankLeaf: null | Group;
+  spellTrapLeaf: null | Group;
   imageLeaf: null;
   maskLeaf: null;
   pendulumLeaf: null;
@@ -164,6 +163,157 @@ export class YugiohCard extends Card {
       visible:this.showAttribute,
       zIndex:10
     })
+  }
+
+  drawLevel() {
+    if (!this.levelLeaf) {
+      this.levelLeaf = new Group();
+      for (let i = 0; i < 13; i++) {
+        const level = new Image();
+        this.levelLeaf.add(level);
+      }
+      this.leafer?.add(this.levelLeaf);
+    }
+
+    const levelUrl = `${this.baseImage}/level.png`;
+    const levelWidth = 88;
+    const right = (this.data.level??0) < 13 ? 147 : 101;
+    this.levelLeaf.children.forEach((level, index) => {
+      level.set({
+        url: levelUrl,
+        x: this.cardWidth - right - index * (levelWidth + 4),
+        y: 247,
+        around: { x: 1, y: 0 },
+        visible: index < (this.data.level??0),
+      });
+    });
+
+    this.levelLeaf.set({
+      visible: this.showLevel,
+      zIndex: 10,
+    });
+  }
+
+  drawRank() {
+    if (!this.rankLeaf) {
+      this.rankLeaf = new Group();
+      for (let i = 0; i < 13; i++) {
+        const rank = new Image();
+        this.rankLeaf.add(rank);
+      }
+      this.leafer?.add(this.rankLeaf);
+    }
+
+    const rankUrl = `${this.baseImage}/rank.png`;
+    const rankWidth = 88;
+    const left = (this.data.rank??0) < 13 ? 147 : 101;
+    this.rankLeaf.children.forEach((rank, index) => {
+      rank.set({
+        url: rankUrl,
+        x: left + index * (rankWidth + 4),
+        y: 247,
+        visible: index < (this.data.rank??0),
+      });
+    });
+
+    this.rankLeaf.set({
+      visible: this.showRank,
+      zIndex: 10,
+    });
+  }
+
+  drawSpellTrap() {
+    if (!this.spellTrapLeaf) {
+      this.spellTrapLeaf = new Group();
+
+      const rightText = new CompressText();
+      const spellTrapIcon = new Image();
+      const leftText = new CompressText();
+
+      this.spellTrapLeaf.add(rightText);
+      this.spellTrapLeaf.add(spellTrapIcon);
+      this.spellTrapLeaf.add(leftText);
+
+      this.leafer?.add(this.spellTrapLeaf);
+    }
+
+    const { spellTrap } = this.style;
+    const { icon } = spellTrap as StyleObject;
+
+    const iconUrl = this.data.icon ? `${this.baseImage}/icon-${this.data.icon}.png` : '';
+    const iconWidth = this.data.icon ? 72 : 0;
+    const leftBracket = ['en', 'kr'].includes(this.data.language??"") ? '[' : '【';
+    const rightBracket = ['en', 'kr'].includes(this.data.language??"") ? ']' : '】';
+    const letterSpacing = (spellTrap as StyleObject).letterSpacing as number || 0;
+    const wordSpacing = (spellTrap as StyleObject).wordSpacing || 0;
+
+    const rightText = this.spellTrapLeaf.children[0];
+    const spellTrapIcon = this.spellTrapLeaf.children[1];
+    const leftText = this.spellTrapLeaf.children[2];
+
+    rightText.set({
+      text: rightBracket,
+      fontFamily: (spellTrap as StyleObject).fontFamily as string,
+      fontSize: (spellTrap as StyleObject).fontSize as number,
+      letterSpacing,
+      wordSpacing,
+      scaleY: (spellTrap as StyleObject).scaleY as number || 1,
+      y: (spellTrap as StyleObject).top as number,
+      key: this.key,
+    });
+    const rightBounds = rightText.noBounds;
+    rightText.x = this.cardWidth - ((spellTrap as StyleObject).right as number) - rightBounds.width;
+
+    spellTrapIcon.set({
+      url: iconUrl,
+      x: rightText.x - (this.data.icon ? (icon.marginRight || 0) : 0) - iconWidth,
+      y: spellTrap.top + (icon.marginTop || 0),
+    });
+
+    leftText.set({
+      text: leftBracket + this.spellTrapName,
+      fontFamily: spellTrap.fontFamily,
+      fontSize: spellTrap.fontSize,
+      letterSpacing,
+      wordSpacing,
+      scaleY: spellTrap.scaleY || 1,
+      rtFontSize: spellTrap.rtFontSize,
+      rtTop: spellTrap.rtTop,
+      rtFontScaleX: spellTrap.rtFontScaleX || 1,
+      y: spellTrap.top,
+      key: this.key,
+    });
+    const leftBounds = leftText.bounds;
+    leftText.x = spellTrapIcon.x - (this.data.icon ? (icon.marginLeft || 0) : 0) - leftBounds.width;
+
+    this.spellTrapLeaf.set({
+      visible: ['spell', 'trap'].includes(this.data.type),
+      zIndex: 10,
+    });
+  }
+
+  get showRank() {
+    let show = false;
+    if (this.data.type === 'monster') {
+      show = this.data.cardType === 'xyz';
+    } else if (this.data.type === 'pendulum') {
+      show = this.data.pendulumType === 'xyz-pendulum';
+    }
+    return show;
+  }
+
+  get showEffect() {
+    return ['monster', 'pendulum'].includes(this.data.type??"") && this.data.monsterType;
+  }
+
+  get showLevel() {
+    let show = false;
+    if (this.data.type === 'monster') {
+      show = ['normal', 'effect', 'ritual', 'fusion', 'synchro', 'token'].includes(this.data.cardType??"");
+    } else if (this.data.type === 'pendulum') {
+      show = ['normal-pendulum', 'effect-pendulum', 'ritual-pendulum', 'fusion-pendulum', 'synchro-pendulum'].includes(this.data.pendulumType);
+    }
+    return show;
   }
 
   get attributeUrl() {
